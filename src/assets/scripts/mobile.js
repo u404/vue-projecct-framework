f1158432b4cd4af305289316dc29422213999e09
zhangguanghui <zhangguanghui@smartcinema.com.cn>
2018-10-24 14:59:14 +0800

    调整了mobile 对rem计算的支持，另外，增加了extend.js中的trim方法

--- src/assets/scripts/mobile.js
+++ src/assets/scripts/mobile.js
@@ -1,2 +1,10 @@
 const designWidth = 750
-document.documentElement.style.fontSize = document.documentElement.clientWidth * 100 / designWidth + 'px'
+const maxPageWidth = 500
+
+function responsive () {
+  let clientWidth = document.documentElement.clientWidth > maxPageWidth ? maxPageWidth : document.documentElement.clientWidth
+  document.documentElement.style.fontSize = clientWidth * 100 / designWidth * 2 + 'px'
+}
+
+responsive()
+window.onresize = responsive
